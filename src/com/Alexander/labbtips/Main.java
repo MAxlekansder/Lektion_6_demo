2497733581a0576a998c9c7d242d044d4e4b9b54
MAxlekansder <144068201+MAxlekansder@users.noreply.github.com>
2023-09-28 10:41:59 +0200

    switch branch

--- src/com/Alexander/labbtips/Main.java
+++ src/com/Alexander/labbtips/Main.java
@@ -3,14 +3,7 @@ package com.Alexander.labbtips;
 public class Main {
     public static void main(String[] args) {
 
-
-        try {
-            System.out.println("dwadwa");
-            System.out.println(2/0);
-        } catch (Exception e) {
-            System.out.println("inne");
-            e.printStackTrace();
-        }
+        //test branch
 
     }
 }
